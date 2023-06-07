46bb3637157bfd85b423927749c84103fbabfd12
Jensone <hello@jensone.com>
2023-06-07 15:20:27 +0200

    🎉 Blog Xpress started !

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'Homepage',
+        ]);
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'Homepage',
+        ]);
+    }
+}
