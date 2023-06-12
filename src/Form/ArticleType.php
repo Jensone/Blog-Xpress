bbca6cd830a678a10cad925cc2d54d284c0c4c5f
Jensone <hello@jensone.com>
2023-06-12 11:11:15 +0200

    🚑 Prepare for clone and setting up

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +16,21 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('content')
-            ->add('image')
-            ->add('createdAt')
-            ->add('featured')
-            ->add('isPremium')
-            ->add('category')
+            ->add('title', TextType::class)
+            ->add('slug', TextType::class)
+            ->add('content', CKEditorType::class, array(
+                'config' => array(
+                    'uiColor' => '#ffffff',
+                ),
+            ))
+            ->add('image', TextType::class)
+            ->add('createdAt', DateTimeType::class)
+            ->add('featured', CheckboxType::class)
+            ->add('isPremium', CheckboxType::class)
+            ->add('category', null, [
+                'choice_label' => 'name',
+                'placeholder' => 'Choisir une catégorie'
+            ])
         ;
     }
 
