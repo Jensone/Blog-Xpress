65a2b3ae589e93b013bd96b155c594df96a37268
Jensone <hello@jensone.com>
2023-06-13 23:55:06 +0200

    🔥 refactor(HomepageController.php): remove unused import statement 🎨 style(article.html.twig): change page title to match content and add gradient background to header 🎨 style(articles.html.twig): change publication to publie and remove separator before featured and premium icons

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,13 +5,13 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
-use PhpParser\Node\Expr\Cast;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomepageController extends AbstractController
 {
+    // Route pour afficher la page d'accueil
     #[Route('/', name: 'homepage')]
     public function index(
         CategoryRepository $categories,
